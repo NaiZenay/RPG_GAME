ed90520dd622cb609c55117f6046ddf5e83d692b
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-03-16 13:35:21 -0600

    Armas

---
+++ Artefacts/Wepons/Wepons.h
@@ -0,0 +1,15 @@
+//
+// Created by Ian on 3/15/2024.
+//
+
+#ifndef RPG_GAME_WEPONS_H
+#define RPG_GAME_WEPONS_H
+
+#include "../Artefacts.h"
+
+class Wepons :Artefacts{
+    int damage;
+};
+
+
+#endif //RPG_GAME_WEPONS_H
