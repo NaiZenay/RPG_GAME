6bb49f22f93c042d1f1eb8e2e6934d27746bd20b
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-03-16 13:44:42 -0600

    Caballero

---
+++ Class/Knight/Knight.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ian on 3/16/2024.
+//
+
+#include "Knight.h"
---
+++ Class/Knight/Knight.h
@@ -0,0 +1,17 @@
+//
+// Created by Ian on 3/16/2024.
+//
+
+#ifndef RPG_GAME_KNIGHT_H
+#define RPG_GAME_KNIGHT_H
+#include "../../Player/Player.h"
+
+class Knight {
+private:
+    Player player;
+public:
+    Knight(Player _player){}
+};
+
+
+#endif //RPG_GAME_KNIGHT_H
